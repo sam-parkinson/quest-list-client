353be78336dfc38a312d7431f4bb18d095646b35
Sam Parkinson <parkinsonsp42@gmail.com>
2019-08-13 12:32:15 -0400

    Force logout upon closing app

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -35,6 +35,7 @@ class App extends Component {
   componentWillUnmount() {
     IdleService.unRegisterIdleResets();
     TokenService.clearCallbackBeforeExpiry();
+    TokenService.clearAuthToken();
   }
 
   logoutFromIdle = () => {
